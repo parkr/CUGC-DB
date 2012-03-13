b5f5d09ee78cd7ff1833216cfa7c474f9fd391a2
Parker Moore <parkrmoore@gmail.com>
2012-03-13 15:27:28 -0400

    Can now mass-edit users.

--- app/Controller/MembersController.php
+++ app/Controller/MembersController.php
@@ -39,98 +39,9 @@ class MembersController extends AppController {
  */
 	public function add() {
 		if ($this->request->is('post')) {
-			
-			// Parse emails
-			$emails = explode(',', $this->request->data['Email']['email']);
-			unset($this->request->data['Email']['email']);
-			$i = 0;
-			foreach($emails as $email){
-				$this->request->data['Email'][$i] = array(
-					'email' => trim($email)
-				);
-				$i++;
-			}
-			// Parse phone numbers
-			$phone_numbers = explode(',', $this->request->data['PhoneNumber']['phone_number']);
-			unset($this->request->data['PhoneNumber']['phone_number']);
-			$i = 0;
-			foreach($phone_numbers as $phone_number){
-				$this->request->data['PhoneNumber'][$i] = array(
-					'phone_number' => trim($phone_number)
-				);
-				$i++;
-			}
-			// Parse degrees, years
-			$grad['degrees'] = explode(',', $this->request->data['GraduationYear']['degree']);
-			unset($this->request->data['GraduationYear']['degree']);
-			$grad['years'] = explode(',', $this->request->data['GraduationYear']['year']);
-			unset($this->request->data['GraduationYear']['year']);
-			
-			$i = 0;
-			foreach($grad['degrees'] as $graddegree){
-				$this->request->data['GraduationYear'][$i] = array(
-					'degree' => trim($graddegree),
-					'year' => trim(@$grad['years'][$i])
-				);
-				$i++;
-			}
-			// Parse mailing addresses, greater cities, states
-			$addresses['mailing_addresses'] = explode(';', $this->request->data['MailingAddress']['mailing_address']);
-			unset($this->request->data['MailingAddress']['mailing_address']);
-			
-			$addresses['greater_cities'] = explode(';', $this->request->data['MailingAddress']['greater_city']);
-			if(count($addresses['greater_cities']) == 1 && count($addresses['mailing_addresses']) > 1){
-				$addresses['greater_cities'] = explode(',', $this->request->data['MailingAddress']['greater_city']);
-			}
-			unset($this->request->data['MailingAddress']['greater_city']);
-			
-			$addresses['states'] = explode(';', $this->request->data['MailingAddress']['state']);
-			if(count($addresses['states']) == 1 && count($addresses['mailing_addresses']) > 1){
-				$addresses['states'] = explode(',', $this->request->data['MailingAddress']['state']);
-			}
-			unset($this->request->data['MailingAddress']['state']);
-			
-			$i = 0;
-			foreach($addresses['mailing_addresses'] as $mailing_address){
-				$this->request->data['MailingAddress'][$i] = array(
-					'mailing_address' => trim($mailing_address),
-					'greater_city' => trim(@$addresses['greater_cities'][$i]),
-					'state' => trim(@$addresses['states'][$i]),
-				);
-				$i++;
-			}
-			// Parse occupations: positions, years
-			$occupations['positions'] = explode(',', $this->request->data['Occupation']['position']);
-			unset($this->request->data['Occupation']['position']);
-			$occupations['companies'] = explode(',', $this->request->data['Occupation']['company']);
-			unset($this->request->data['Occupation']['company']);
-			
-			$i = 0;
-			foreach($occupations['companies'] as $company){
-				$this->request->data['Occupation'][$i] = array(
-					'position' => trim(@$occupations['positions'][$i]),
-					'company' => trim($company),
-				);
-				$i++;
-			}
-			// Parse officer positions: positions, years
-			// Parse occupations: positions, years
-			$officerpositions['positions'] = explode(',', $this->request->data['OfficerPosition']['position']);
-			unset($this->request->data['OfficerPosition']['position']);
-			$officerpositions['years'] = explode(',', $this->request->data['OfficerPosition']['years']);
-			unset($this->request->data['OfficerPosition']['years']);
-			
-			$i = 0;
-			foreach($officerpositions['positions'] as $position){
-				$this->request->data['OfficerPosition'][$i] = array(
-					'position' => trim($position),
-					'company' => trim(@$officerpositions['years'][$i]),
-				);
-				$i++;
-			}
-			
+			$this->request->data = $this->_parseMemberAssociatedFields($this->request->data);
 			$this->Member->create();
-			if ($this->Member->saveAssociated($this->request->data)) {
+			if ($this->Member->saveAll($this->request->data)) {
 				$this->Session->setFlash(__('The member has been saved'));
 				$this->redirect(array('action' => 'index'));
 			} else {
@@ -151,11 +62,18 @@ class MembersController extends AppController {
 			throw new NotFoundException(__('Invalid member'));
 		}
 		if ($this->request->is('post') || $this->request->is('put')) {
-			if ($this->Member->save($this->request->data)) {
+			$this->Member->Email->deleteAll(array('Email.member_id' => $id), false);
+			$this->Member->PhoneNumber->deleteAll(array('PhoneNumber.member_id' => $id), false);
+			$this->Member->MailingAddress->deleteAll(array('MailingAddress.member_id' => $id), false);
+			$this->Member->Occupation->deleteAll(array('Occupation.member_id' => $id), false);
+			$this->Member->OfficerPosition->deleteAll(array('OfficerPosition.member_id' => $id), false);
+			$this->Member->GraduationYear->deleteAll(array('GraduationYear.member_id' => $id), false);
+			if ($this->Member->saveAll($this->request->data)) {
 				$this->Session->setFlash(__('The member has been saved'));
 				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The member could not be saved. Please, try again.'));
+				$this->redirect(array('action' => 'view', $id));
 			}
 		} else {
 			$this->request->data = $this->Member->read(null, $id);
@@ -182,5 +100,96 @@ class MembersController extends AppController {
 		}
 		$this->Session->setFlash(__('Member was not deleted'));
 		$this->redirect(array('action' => 'index'));
+	}
+	
+	private function _parseMemberAssociatedFields($data){
+		// Parse emails
+		$emails = explode(',', $data['Email']['email']);
+		unset($data['Email']['email']);
+		$i = 0;
+		foreach($emails as $email){
+			$data['Email'][$i] = array(
+				'email' => trim($email)
+			);
+			$i++;
+		}
+		// Parse phone numbers
+		$phone_numbers = explode(',', $data['PhoneNumber']['phone_number']);
+		unset($data['PhoneNumber']['phone_number']);
+		$i = 0;
+		foreach($phone_numbers as $phone_number){
+			$data['PhoneNumber'][$i] = array(
+				'phone_number' => trim($phone_number)
+			);
+			$i++;
+		}
+		// Parse degrees, years
+		$grad['degrees'] = explode(',', $data['GraduationYear']['degree']);
+		unset($data['GraduationYear']['degree']);
+		$grad['years'] = explode(',', $data['GraduationYear']['year']);
+		unset($data['GraduationYear']['year']);
+			
+		$i = 0;
+		foreach($grad['degrees'] as $graddegree){
+			$data['GraduationYear'][$i] = array(
+				'degree' => trim($graddegree),
+				'year' => trim(@$grad['years'][$i])
+			);
+			$i++;
+		}
+		// Parse mailing addresses, greater cities, states
+		$addresses['mailing_addresses'] = explode(';', $data['MailingAddress']['mailing_address']);
+		unset($data['MailingAddress']['mailing_address']);
+			
+		$addresses['greater_cities'] = explode(';', $data['MailingAddress']['greater_city']);
+		if(count($addresses['greater_cities']) == 1 && count($addresses['mailing_addresses']) > 1){
+			$addresses['greater_cities'] = explode(',', $data['MailingAddress']['greater_city']);
+		}
+		unset($data['MailingAddress']['greater_city']);
+			
+		$addresses['states'] = explode(';', $data['MailingAddress']['state']);
+		if(count($addresses['states']) == 1 && count($addresses['mailing_addresses']) > 1){
+			$addresses['states'] = explode(',', $data['MailingAddress']['state']);
+		}
+		unset($data['MailingAddress']['state']);
+			
+		$i = 0;
+		foreach($addresses['mailing_addresses'] as $mailing_address){
+			$data['MailingAddress'][$i] = array(
+				'mailing_address' => trim($mailing_address),
+				'greater_city' => trim(@$addresses['greater_cities'][$i]),
+				'state' => trim(@$addresses['states'][$i]),
+			);
+			$i++;
+		}
+		// Parse occupations: positions, years
+		$occupations['positions'] = explode(',', $data['Occupation']['position']);
+		unset($data['Occupation']['position']);
+		$occupations['companies'] = explode(',', $data['Occupation']['company']);
+		unset($data['Occupation']['company']);
+			
+		$i = 0;
+		foreach($occupations['companies'] as $company){
+			$data['Occupation'][$i] = array(
+				'position' => trim(@$occupations['positions'][$i]),
+				'company' => trim($company),
+			);
+			$i++;
+		}
+		// Parse officer positions: positions, years
+		$officerpositions['positions'] = explode(',', $data['OfficerPosition']['position']);
+		unset($data['OfficerPosition']['position']);
+		$officerpositions['years'] = explode(',', $data['OfficerPosition']['years']);
+		unset($data['OfficerPosition']['years']);
+			
+		$i = 0;
+		foreach($officerpositions['positions'] as $position){
+			$data['OfficerPosition'][$i] = array(
+				'position' => trim($position),
+				'years' => trim(@$officerpositions['years'][$i]),
+			);
+			$i++;
+		}
+		return $data;
 	}
 }
--- app/View/Members/edit.ctp
+++ app/View/Members/edit.ctp
@@ -1,20 +1,6 @@
-<div class="members form">
-<?php echo $this->Form->create('Member');?>
-	<fieldset>
-		<legend><?php echo __('Edit Member'); ?></legend>
-	<?php
-		echo $this->Form->input('id');
-		echo $this->Form->input('first_name');
-		echo $this->Form->input('last_name');
-		echo $this->Form->input('things_to_note');
-	?>
-	</fieldset>
-<?php echo $this->Form->end(__('Submit'));?>
-</div>
 <div class="actions">
 	<h3><?php echo __('Actions'); ?></h3>
 	<ul>
-
 		<li><?php echo $this->Form->postLink(__('Delete'), array('action' => 'delete', $this->Form->value('Member.id')), null, __('Are you sure you want to delete # %s?', $this->Form->value('Member.id'))); ?></li>
 		<li><?php echo $this->Html->link(__('List Members'), array('action' => 'index'));?></li>
 		<li><?php echo $this->Html->link(__('List Emails'), array('controller' => 'emails', 'action' => 'index')); ?> </li>
@@ -32,4 +18,39 @@
 		<li><?php echo $this->Html->link(__('List Phone Numbers'), array('controller' => 'phone_numbers', 'action' => 'index')); ?> </li>
 		<li><?php echo $this->Html->link(__('New Phone Number'), array('controller' => 'phone_numbers', 'action' => 'add')); ?> </li>
 	</ul>
+</div>
+<div class="members form">
+<?php echo $this->Form->create('Member');?>
+	<fieldset>
+		<legend><?php echo __('Edit Member'); ?></legend>
+	<?php
+		echo $this->Form->input('id');
+		echo $this->Form->input('first_name');
+		echo $this->Form->input('last_name');
+		
+		// List associated values, +1
+		for($i=0; $i<count($this->Form->data['Email']); $i++){ echo $this->Form->input("Email.$i.email", array('label' => "Email #".($i+1))); }
+		for($i=0; $i<count($this->Form->data['PhoneNumber']); $i++){ echo $this->Form->input("PhoneNumber.$i.phone_number", array('label' => "Phone Number #".($i+1))); }
+		for($i=0; $i<count($this->Form->data['MailingAddress']); $i++){
+			echo $this->Form->input("MailingAddress.$i.mailing_address", array('label' => "Mailing Address #".($i+1), 'type' => 'text'));
+			echo $this->Form->input("MailingAddress.$i.greater_city", array('label' => "Greater City #".($i+1), 'type' => 'text'));
+			echo $this->Form->input("MailingAddress.$i.state", array('label' => "State #".($i+1), 'type' => 'text'));
+		}
+		for($i=0; $i<count($this->Form->data['Occupation']); $i++){
+			echo $this->Form->input("Occupation.$i.position", array('label' => "Occupation: Position #".($i+1)));
+			echo $this->Form->input("Occupation.$i.company", array('label' => "Occupation: Company #".($i+1)));
+		}
+		for($i=0; $i<count($this->Form->data['OfficerPosition']); $i++){
+			echo $this->Form->input("OfficerPosition.$i.position", array('label' => "Officer Position #".($i+1)));
+			echo $this->Form->input("OfficerPosition.$i.years", array('label' => "Officer Years #".($i+1)));
+		}
+		for($i=0; $i<count($this->Form->data['GraduationYear']); $i++){
+			echo $this->Form->input("GraduationYear.$i.degree", array('label' => "Degree #".($i+1)));
+			echo $this->Form->input("GraduationYear.$i.year", array('label' => "Graduation Year #".($i+1), 'type' => 'number'));
+		}
+		
+		echo $this->Form->input('things_to_note');
+	?>
+	</fieldset>
+<?php echo $this->Form->end(__('Submit'));?>
 </div>
